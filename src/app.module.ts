86d57e70a4604111d16daf2005b3454d7dddf147
maikofelix47 <maikofelix89@gmail.com>
2023-02-03 20:05:39 +0300

    Added users module, service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { CategoryModule } from './category/category.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ProductModule, CategoryModule],
+  imports: [ProductModule, CategoryModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
