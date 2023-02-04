12ecae54866cfa71b4520b2725427f7d14f755e7
maikofelix47 <maikofelix89@gmail.com>
2023-02-04 19:50:13 +0300

    Added TypeORM to project and user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,26 @@ import { ProductModule } from './product/product.module';
 import { CategoryModule } from './category/category.module';
 import { UsersModule } from './users/users.module';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+//entities
+import { User } from './users/user.entity';
+
 @Module({
-  imports: [ProductModule, CategoryModule, UsersModule],
+  imports: [ProductModule, CategoryModule, UsersModule,
+   TypeOrmModule.forRoot({
+    type: '',
+    host: '',
+    port: 0,
+    username: '',
+    password: '',
+    database: '',
+    logging: true,
+    retryAttempts: 2,
+    autoLoadEntities: true,
+    synchronize: true,
+   })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Param, Body } from '@nestjs/common';
+
+import { UsersService } from './users.service';
+
+import { CreateUserDto } from './dtos/create-user-dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private usersService: UsersService){
+
+    }
+
+     @Get()
+     findAll(){
+        return this.usersService.findAll()
+     }
+    
+     @Get('/:id')
+     findOne(@Param('id') id: number){
+         return this.usersService.findOne(id);
+     }
+
+     @Post()
+     signUpUser(@Body() body: CreateUserDto){
+         const payload = {
+            ...body,
+            isActive: true,
+            id: 0
+         };
+        return this.usersService.create(payload);
+     }
+
+
+
+}
