f08dd1319a489f012e9a5da6bae105da44df3b2b
maikofelix47 <maikofelix89@gmail.com>
2023-02-09 20:02:36 +0300

    Added association between role and permission

--- src/permission/permission-entity.ts
+++ src/permission/permission-entity.ts
@@ -1,35 +1,47 @@
-import { Column, CreateDateColumn, Entity, Generated, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Generated,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  ManyToOne
+} from 'typeorm';
+
+import { Role } from '../role/role.entity';
 
 @Entity()
-export class Permission{
-@PrimaryGeneratedColumn()
-id: number;
+export class Permission {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-@Column()
-name: string;
+  @Column()
+  name: string;
 
-@Column({nullable: true})
-description: string;
+  @Column({ nullable: true })
+  description: string;
 
-@Generated('uuid')
-uuid: string;
+  @Generated('uuid')
+  uuid: string;
 
-@CreateDateColumn()
-dateCreated: Date;
+  @CreateDateColumn()
+  dateCreated: Date;
 
-@UpdateDateColumn()
-dateUpdated: Date;
+  @UpdateDateColumn()
+  dateUpdated: Date;
 
-@Column({nullable: true})
-voided: boolean;
+  @Column({ nullable: true })
+  voided: boolean;
 
-@Column({nullable: true})
-voidedBy: number;
+  @Column({ nullable: true })
+  voidedBy: number;
 
-@Column({nullable: true})
-voidedDate: Date;
+  @Column({ nullable: true })
+  voidedDate: Date;
 
-@Column({nullable: true})
-voidedReason: string;
+  @Column({ nullable: true })
+  voidedReason: string;
 
+  @ManyToOne(()=> Role, (role)=> role.permissions)
+  roles: Role[]
 }
--- src/role/role.entity.ts
+++ src/role/role.entity.ts
@@ -1,35 +1,48 @@
-import { Column, CreateDateColumn, Entity, Generated, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Generated,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  OneToMany
+} from 'typeorm';
+
+import { Permission } from '../permission/permission-entity';
 
 @Entity()
-export class Role{
-    @PrimaryGeneratedColumn()
-    id: number;
+export class Role {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    name: string;
+  @Column()
+  name: string;
 
-    @Column()
-    description: string;
+  @Column()
+  description: string;
 
-    @Column()
-    @Generated('uuid')
-    uuid: string;
+  @Column()
+  @Generated('uuid')
+  uuid: string;
 
-    @CreateDateColumn({nullable: false})
-    dateCreated: Date;
+  @CreateDateColumn({ nullable: false })
+  dateCreated: Date;
 
-    @UpdateDateColumn({ nullable: true})
-    dateUpdated: Date;
+  @UpdateDateColumn({ nullable: true })
+  dateUpdated: Date;
 
-    @Column({nullable: true})
-    voided: boolean;
+  @Column({ nullable: true })
+  voided: boolean;
 
-    @Column({ nullable: true, type: 'timestamp' })
-    voidedDate: Date;
-  
-    @Column({ nullable: true})
-    voidedBy: number;
-  
-    @Column({ nullable: true})
-    voidedReason: string;
+  @Column({ nullable: true, type: 'timestamp' })
+  voidedDate: Date;
+
+  @Column({ nullable: true })
+  voidedBy: number;
+
+  @Column({ nullable: true })
+  voidedReason: string;
+
+  @OneToMany(()=> Permission, (permission)=> permission.roles)
+  permissions: Permission[]
 }
